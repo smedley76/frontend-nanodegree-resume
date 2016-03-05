cc0b33deac63a6d871d934d29d966fdfa394ddbf
Steven Medley <smedley76@gmail.com>
2016-03-05 00:05:21 -0600

    Lesson 1: DATA TYPES > resume header     Watch out here, they try to trick you, watch your videos     and read the instructors notes.       SPOILER: you need to have a total of 4 variables.       var neededVars = {         ["name", "role", "formattedName", "formattedRole"]       }; //     ^---<< They also try to confuse you here, sometimes they //            use it and sometimes they dont..

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
-var awesomeThoughts = "I am Steven and I am Awesome!";
-var funThoughts = awesomeThoughts.replace("Awesome", "FUN");
-$("#main").append(funThoughts);
+//goodbye fun thoughts, and awesome thoughts.... ill miss you....
+
+var name = "Steven Medley"
+var role = "Wannabe Developer!"
+var formattedName = 
+  HTMLheaderName.replace("%data%", name);
+var formattedRole =
+  HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedName, formattedRole);
