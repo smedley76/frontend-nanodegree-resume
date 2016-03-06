56e3874c1676d4a12a94d1ddb5aa1ef26d44cd66
Steven Medley <smedley76@gmail.com>
2016-03-05 20:52:06 -0600

    Lesson 1: Data Types > Biography Quiz     Dang this was a pain figuring out what     they really wanted from you, they dont     give a lot of information other than     "do this, now  append it", really making     you have to ramsack the source to figure it     all out.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,48 @@
-//goodbye fun thoughts, and awesome thoughts.... ill miss you....
+/*
+I feel like i cheated by looking in the helper.js file
+but you really have to to see all the HTMLxxxxx stuff
+the reason i feel i cheated, is because the instructor notes dont
+say anything about adding "location" in the contacts (which i added)
+nor does the video. also they dont exactly tell you how to append 
+anything where it goes, perhaps thats part of the learning process?
+another thing im confused about, if i *pend skills to #header the infamous
+list item bullets show up, but ordered in the correct area of the DOM however if i
+*pend to #skills it of course removes them but then the stretch across the page.
+i know how i can fix it, but maybe i didnt look deep enough to figure out whats
+going to fix it.
+*/ 
 
-var name = "Steven Medley"
-var role = "Wannabe Developer!"
-var formattedName = 
-  HTMLheaderName.replace("%data%", name);
-var formattedRole =
-  HTMLheaderRole.replace("%data%", role);
-$("#header").prepend(formattedName, formattedRole);
+//doesnt make sense to type it twice in the same object
+username = "smedley76";
+
+var bio = {
+  "name": "Steven Medley",
+  "role": "Wannabe Developer",
+  "contacts": {
+    "mobile": "(816)456-0886",
+    "github": username,
+    "email": username + "@gmail.com",
+    "location": "Kansas City"
+  },
+  "biopic": "images/me.jpg",
+  "welcomemessage": "Student of web development, studying:",
+  "skills": ["C", "CSS" , "HTML", "JavaScript"]
+};
+
+// I *pended everything readable and OCD like organized.
+$("#header").prepend(HTMLheaderName.replace("%data%", bio.name),
+                     HTMLheaderRole.replace("%data%", bio.role));
+
+$("#header").append(HTMLbioPic.replace("%data%", bio.biopic),
+                    HTMLwelcomeMsg.replace("%data%", bio.welcomemessage),
+                    HTMLskillsStart);
+
+
+$("#topContacts").prepend(HTMLmobile.replace("%data%", bio.contacts.mobile),
+                          HTMLlocation.replace("%data%", bio.contacts.location),
+                          HTMLemail.replace("%data%", bio.contacts.email), 
+                          HTMLgithub.replace("%data%", bio.contacts.github));
+$("#skills").prepend(HTMLskills.replace("%data%", bio.skills[0]),
+                     HTMLskills.replace("%data%", bio.skills[1]),
+                     HTMLskills.replace("%data%", bio.skills[2]),
+                     HTMLskills.replace("%data%", bio.skills[3]));
