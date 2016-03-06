1167afbd2499534747e07a7608363fc3dbddadf9
Steven Medley <smedley76@gmail.com>
2016-03-06 13:12:01 -0600

    Lesson 1: Data Types > Practice with Objects Quiz     everything is showing up, skills still arent showing up as pictured.     i know skills have to be *pended to #skills because HTMLskillsStart     contains <ul id=skills> and the CSS turns the bullets off for that     ul ID.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,20 +1,13 @@
 /*
-I feel like i cheated by looking in the helper.js file
-but you really have to to see all the HTMLxxxxx stuff
-the reason i feel i cheated, is because the instructor notes dont
-say anything about adding "location" in the contacts (which i added)
-nor does the video. also they dont exactly tell you how to append 
-anything where it goes, perhaps thats part of the learning process?
-another thing im confused about, if i *pend skills to #header the infamous
-list item bullets show up, but ordered in the correct area of the DOM however if i
-*pend to #skills it of course removes them but then the stretch across the page.
-i know how i can fix it, but maybe i didnt look deep enough to figure out whats
-going to fix it.
+Still having issues with skills, they are *pended to #skills without bullets
+but arent lined up where it should be, im missing something and i cant find it yet
+perhaps its in a furture lesson, everything else looks correct.
 */ 
 
 //doesnt make sense to type it twice in the same object
 username = "smedley76";
 
+//Biography object, JSON
 var bio = {
   "name": "Steven Medley",
   "role": "Wannabe Developer",
@@ -28,21 +21,45 @@ var bio = {
   "welcomemessage": "Student of web development, studying:",
   "skills": ["C", "CSS" , "HTML", "JavaScript"]
 };
-
 // I *pended everything readable and OCD like organized.
 $("#header").prepend(HTMLheaderName.replace("%data%", bio.name),
                      HTMLheaderRole.replace("%data%", bio.role));
-
 $("#header").append(HTMLbioPic.replace("%data%", bio.biopic),
                     HTMLwelcomeMsg.replace("%data%", bio.welcomemessage),
                     HTMLskillsStart);
+$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile),
+                         HTMLlocation.replace("%data%", bio.contacts.location),
+                         HTMLemail.replace("%data%", bio.contacts.email), 
+                         HTMLgithub.replace("%data%", bio.contacts.github));
+$("#skills").append(HTMLskills.replace("%data%", bio.skills.join("<br>")));
 
 
-$("#topContacts").prepend(HTMLmobile.replace("%data%", bio.contacts.mobile),
-                          HTMLlocation.replace("%data%", bio.contacts.location),
-                          HTMLemail.replace("%data%", bio.contacts.email), 
-                          HTMLgithub.replace("%data%", bio.contacts.github));
-$("#skills").prepend(HTMLskills.replace("%data%", bio.skills[0]),
-                     HTMLskills.replace("%data%", bio.skills[1]),
-                     HTMLskills.replace("%data%", bio.skills[2]),
-                     HTMLskills.replace("%data%", bio.skills[3]));
+//Work object, DOT notation, bracket append
+var work = {};
+work.position = "Assembler; Truck Chassis";
+work.employer = "Ford Motor Company";
+work.years = "2015 - present";
+work.city = "Claycomo, MO";
+work.description = "Install front break line on every F150, F150 4x4's install half shafts";
+$("#workExperience").append(HTMLworkStart,
+                            HTMLworkEmployer.replace("%data%", work["employer"]),
+                            HTMLworkTitle.replace("%data%", work["position"]),
+                            HTMLworkDates.replace("%data%", work["years"]),
+                            HTMLworkLocation.replace("%data%", work["city"]),
+                            HTMLworkDescription.replace("%data%", work["description"]));
+
+
+//Education, BRACKET notation, dot append
+var education = {};
+education["school"] = "Excelsior Springs Job Corps";
+education["degree"] = ["G.E.D.", "HS diploma", "SMAW cert", "GMAW cert", "FCAW cert"]; 
+education["years"] = "2006 - 2007";
+education["city"] = "Excelsior Springs, MO";
+education["major"] = "Welding";
+$("#education").append(HTMLschoolStart,
+                HTMLschoolName.replace("%data%", education.school),
+                HTMLschoolDegree.replace("%data%", education.degree),
+                HTMLschoolDates.replace("%data%", education.years),
+                HTMLschoolLocation.replace("%data%", education.city),
+                HTMLschoolMajor.replace("%data%", education.major));
+
